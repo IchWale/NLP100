5c2c12e18667c1784a92755954be7ebc763c7d5e
Kujira <t.kousokabe@gmail.com>
2016-10-13 16:07:19 +0900

    add 06

--- 06/nlp059.py
+++ 06/nlp059.py
@@ -7,22 +7,21 @@
 import xml.etree.ElementTree as ET
 import re
 
+
 def parse_exp(text, exp_list):
     """
     S式をパーザする関数. 与えられたテキスト中のすべてのS式をリストとして返す.
-    一対の()で閉じている式を一旦{}に置換し、置換後のテキストを入力として再帰的に処理.
-    式として確定した部分は{}を()に直してリストに格納
+    一対の()で閉じている式を{}に置換し、置換後のテキストを入力として再帰的に処理.
+    式として確定した部分は()が{}になった状態でリストに格納される
     args:
         text: str, S式のテキスト
-        exp_list, list, S式のリスト
+        exp_list, list, {}に置換したS式のリスト
     return:
-        exp_list, list, S式のリスト
+        exp_list, list, {}に置換したS式のリスト
     """
     pattern = re.compile(r"\(([^()]+)\)")
-    pattern_s = re.compile(r"{(.+?)}")
     if re.search(pattern, text):
-        rep = lambda s: "(" + s.translate(str.maketrans("{}", "()")) + ")"
-        exp_list.extend([rep(s) for s in re.findall(pattern, text)])
+        exp_list.extend(["{"+s+"}" for s in re.findall(pattern, text)])
         rep_text = re.sub(pattern, "{\g<1>}", text)
         exp_list = parse_exp(rep_text, exp_list)
     return exp_list
@@ -34,18 +33,25 @@ def extract_np(elem):
     args:
         elem: ETオブジェクト, Stanford Core NLPの解析結果xmlのETパーザオブジェクト
     """
-    pattern = re.compile(r"\([A-Z]+ ([^()]+)\)")
+    pattern_w = re.compile(r"{[A-Z]+ ([^{}]+)}")
     for sentence in elem.find(".//sentences"):
         s_exp = sentence.find("parse").text
         exp_list = parse_exp(s_exp, [])
         print("--------sentence id :{}--------".format(sentence.get("id")))
-
         for exp in exp_list:
-            if exp[0:3] == "(NP":
-                print(" ".join(re.findall(pattern, exp)))
+            if exp[0:3] == "{NP":
+                # print(exp)
+                print(" ".join(re.findall(pattern_w, exp)))
 
         
 if __name__ == '__main__':
     tree = ET.parse("nlp.txt.xml")
     elem = tree.getroot()
     extract_np(elem)
+
+"""
+めも
+
+parse_exp()は簡潔さを優先で記述
+再帰処理+置換で処理速度はいまいちかもしれない
+"""
